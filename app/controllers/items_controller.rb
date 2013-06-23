498752ecd548ce57b04f195130834e29ebcd4e94
Alessandro Tegner <alessandro.tegner@gmail.com>
2013-06-23 22:49:12 +0000

    Fix

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < InheritedResources::Base
   self.responder = AppResponder
   actions :index, :show
-  respond_to :html, :json, :xml, only: :index
-  respond_to :json, :xml, only: :show
+
+  respond_to :json, :xml
 end
